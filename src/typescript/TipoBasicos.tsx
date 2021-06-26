296bb771d75b1f59820886b88624e83425310c7a
Kevin Martinez <kkevinmartinez7@gmail.com>
2021-06-26 13:10:59 -0600

    reinforcement 2 type of variables

--- src/typescript/TipoBasicos.tsx
+++ src/typescript/TipoBasicos.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 export const TipoBasicos = () => {
+    const nombre: string | number = 'Kevin';
+    const edad: number = 35;
+    const estaActivo: boolean = true;
+    const poderes: string[] = ['Velocidad', 'Volar', 'Respirar en el agua'];
+
     return (
         <>
             <h3>Tipos Basicos</h3>
+            {nombre} - {edad}, {estaActivo ? 'activo' : 'inactivo'}
+            <br/>
+            {poderes.join(', ')}
         </>
     )
 }
